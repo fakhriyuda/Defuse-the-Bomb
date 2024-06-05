94943e34148fbb8b42765ebeef06e0ce52b1eefb
Pak Aduy <164998008+dev-mob-superfk@users.noreply.github.com>
2024-06-05 15:06:27 +0700

    Initial Commit

---
+++ Defuse the Bomb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Defuse the Bomb
+//
+//  Created by ISYS Macbook air 1 on 05/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
